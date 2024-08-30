d40b6a4517783cd0349b9947d388f68cdeb3df20
ENCLD <einar.alme@protonmail.com>
2024-08-30 14:28:57 +0200

    Update 1.3

--- IS-114.py
+++ IS-114.py
@@ -22,11 +22,25 @@ else:
 
 #----------------------------
 
-ice1 = input("Favorite ice cream then? ")
+ice1 = input("What's your favorite ice cream? ")
 
 ice1Cap = ice1.capitalize()
 
 input((ice1Cap) + " is not a good ice cream. Please give good reason. ")
+
+#math
+
+#----------------------------
+
+math1 = input("Here comes a math question. What is 4 + 4? ")
+
+
+if int(math1) == 8:
+    print("Well done.")
+elif int(math1) <= 0:
+    print("How bad can you be!?")
+else:
+    print("False. You really are bad at math :/")
 
 
 
